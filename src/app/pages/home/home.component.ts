24eadd34b9fb147a6a4e7170376834ee1356a911
Harold.parra <Harold.parra@t-evolvers.com>
2024-03-21 20:26:13 -0500

    page home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatDividerModule} from '@angular/material/divider';
+import { CardSimpleComponent } from '../../atoms/card-simple/card-simple.component';
+import { CardWeComponent } from '../../atoms/card-we/card-we.component';
+import { CardBannerComponent } from '../../molecules/card-banner/card-banner.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatDividerModule, CardSimpleComponent, CardWeComponent, CardBannerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
