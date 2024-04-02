7c99971be3acf9556a0a667e7177e3e32a370695
Gabriel Galvan <gabgh@hotmail.com>
2024-04-01 22:29:08 -0500

    menu y banner web

---
+++ src/app/organisms/menu/menu.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent {
+
+  
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,14 +3,18 @@ import {MatDividerModule} from '@angular/material/divider';
 import { CardSimpleComponent } from '../../atoms/card-simple/card-simple.component';
 import { CardWeComponent } from '../../atoms/card-we/card-we.component';
 import { CardBannerComponent } from '../../molecules/card-banner/card-banner.component';
+import { BannerComponent } from '../../molecules/banner/banner.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatDividerModule, CardSimpleComponent, CardWeComponent, CardBannerComponent],
+  imports: [MatDividerModule, CardSimpleComponent, CardWeComponent, CardBannerComponent, BannerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  public download(event:any){
+    console.log(event)
+  }
 }
