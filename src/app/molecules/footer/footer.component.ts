e815cba562565c164d872da55c409fcd6441f76a
Harold.parra <Harold.parra@t-evolvers.com>
2024-04-26 12:40:34 -0500

    cambio en rutas

--- src/app/molecules/footer/footer.component.ts
+++ src/app/molecules/footer/footer.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TextContainerComponent } from '../../atoms/text-container/text-container.component';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [TextContainerComponent],
+  imports: [TextContainerComponent, RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
+  public url='/';
+
+  constructor(private router: Router) { }
+
+ngOnInit(): void {
+    
+    this.router.events.subscribe(event => {
+      this.url = this.router.url;
+      if (event instanceof NavigationEnd) {
+         window.scrollTo(0, 0); 
+      }
+    });
+  }
 }
